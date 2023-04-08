0bc8650b17019cc0b0919045c231d5de51b836ae
PattenChiou <patten1719@gmail.com>
2023-04-08 18:55:31 +0800

    change key of json to fix KeyError and add .gitignore

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ def getaqi(x):
 	r=requests.get("https://data.epa.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON")
 	content=r.json()
 	for i in range(0,len(content["records"])):
-		if content["records"][i]["SiteName"]==x or content["records"][i]["County"]==x :
-			str1+=content["records"][i]["SiteName"]+":"+content["records"][i]["AQI"]+"\n"+"(%s)"%content["records"][i]["PublishTime"]+"\n"
+		if content["records"][i]["sitename"]==x or content["records"][i]["county"]==x :
+			str1+=content["records"][i]["sitename"]+":"+content["records"][i]["aqi"]+"\n"+"(%s)"%content["records"][i]["publishtime"]+"\n"
 	if len(str1)==0:
 		return "查無資料"
 	else:
