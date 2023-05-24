3a5496f1e604b715f31936f0fcf50373211b4bc9
PattenChiou <patten1719@gmail.com>
2023-05-24 23:44:12 +0800

    test

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def getaqi(x):
 #a=getaqi()
 #print(a)
 def talkToChatGPT(content):
-	message = [{"role" : "user", "content" : "You are a chatbot whose name is Penguin."}, {"role" : "user", "content" : content}]
+	message = [{"role" : "user", "content" : "你是一個心理師"}, {"role" : "user", "content" : content}]
 	requestBody = {"model" : "gpt-3.5-turbo",
             "messages" : message,
             "temperature" : 0.5,
