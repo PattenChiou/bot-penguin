50f71cbcf50dc1d03ffbd92da3d6eb0b8290771d
PattenChiou <patten1719@gmail.com>
2024-10-02 11:55:59 +0800

    change aqi url

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
 def getaqi(x):
 	str1=""
-	r=requests.get("https://data.epa.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON")
+	r=requests.get("https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate desc&format=JSON")
 	content=r.json()
 	for i in range(0,len(content["records"])):
 		if content["records"][i]["sitename"]==x or content["records"][i]["county"]==x :
